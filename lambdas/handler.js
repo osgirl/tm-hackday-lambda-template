e6afc93aaf3bf27c260e3af6f2788992efa22b55
Giuseppe Battista <giuseppe.battista@trinitymirror.com>
2017-11-24 16:54:31 +0000

    [EDIT] polished the code

--- lambdas/handler.js
+++ lambdas/handler.js
@@ -44,7 +44,7 @@ module.exports.upload = (event, context, callback) => {
     const bucket_url = process.env.BUCKET_URL;
     const connectionString = process.env.PG_CONNECTION_STRING;
     const tableName = process.env.PG_TABLE;
-    const staticurl = `${BUCKET_URL}/${fileName}`;
+    const staticurl = `${bucket_url}/${fileName}`;
 
     const params = {
         Body: fileBuffer,
@@ -60,30 +60,15 @@ module.exports.upload = (event, context, callback) => {
     };
 
     const insert_query = `INSERT INTO ${tableName}(Timestamp, BucketKey, Username, Description, FileName, StaticLink) values($1, $2, $3, $4, $5, $6)`
-
     s3.putObject(params, (err, data) => {
+
         if (err) callback(new Error([err.statusCode], [err.message]));
+        callback(null, {
+          statusCode: '200',
+          headers: {'Access-Control-Allow-Origin': '*'},
+          body: JSON.stringify({"message": "Successfully uploaded to S3"})
+      });
 
-        pg.connect(connectionString, (err, client, done) => {
-          if(err) callback(new Error([err.statusCode], res.status(500).json({success: false, data: err} )));
-
-          client.query(insert_query,[timestamp, bucket, username, description, filename, staticurl], (err, result) => {
-
-            if(err) callback(new Error([err.statusCode], [err.message]));
-
-            client.end();
-
-            done();
-
-            callback(null, {
-                  statusCode: '200',
-                  headers: {'Access-Control-Allow-Origin': '*'},
-                  body: JSON.stringify({'data': data})
-              });
-
-            });
-
-        });
     });
   } else {
       callback(null, {
@@ -98,52 +83,18 @@ module.exports.list = (event, context, callback) => {
 
   context.callbackWaitsForEmptyEventLoop = false;
 
-  // const listQuery = `SELECT * FROM ${process.env.PG_TABLE} ORDER BY timestamp DESC LIMIT 20;`;
-  // const client = new pg.Client(process.env.PG_CONNECTION_STRING);
-  // console.log(process.env.PG_CONNECTION_STRING);
-  // console.log('connected?');
-  // client.connect((err, res) => {
-  //   console.log(err);
-  //   console.log(res);
-  // });
-  // console.log('connected!');
-  // const query = client.query('SELECT NOW();', (err, results) => {
-  //    client.end();
-  //    console.log(results);
-  //    console.log(err);
-  //   //  callback(null, {
-  //   //   statusCode: '200',
-  //   //   headers: {'Access-Control-Allow-Origin': '*'},
-  //   //   body: JSON.stringify({"message": results})
-  //   // });
-
-  //   context.succeeded(results);
-    // callback(null, {
-    //   statusCode: '200',
-    //   headers: {'Access-Control-Allow-Origin': '*'},
-    //   body: JSON.stringify({"message": "OK"})
-    // });
-
-
-  // });
-
-  // context.callbackWaitsForEmptyEventLoop = false;
-
-  const connectionString ='postgresql://hackers:hackers_password@hackday-pg-template.ckthmwbknecx.eu-west-1.rds.amazonaws.com:5432/hackers_db';
-  const pg = require('pg')
-  const client = new pg.Client(connectionString);
+  const listQuery = `SELECT * FROM ${process.env.PG_TABLE} ORDER BY timestamp DESC LIMIT 20;`;
+  const client = new pg.Client(process.env.PG_CONNECTION_STRING);
   client.connect();
-  const query = client.query('SELECT NOW();', (err, results) => {
-       client.end();
-       console.log("RESULTS HEEEEREEEEEE");
-       console.log(results);
-       console.log(err);
-       callback(null, {
-        statusCode: '200',
-        headers: {'Access-Control-Allow-Origin': '*'},
-        body: JSON.stringify({"message": results})
-      });
-  });
-  
+  const query = client.query(listQuery, (err, results) => {
+     client.end();
+
+    callback(null, {
+      statusCode: '200',
+      headers: {'Access-Control-Allow-Origin': '*'},
+      body: JSON.stringify({"results": results})
+    });
+
+  });  
   
 }
