49009b2a36dba47bb291ea1ad25e9ba3b0889b23
Giuseppe Battista <giuseppe.battista@trinitymirror.com>
2017-11-27 10:47:32 +0000

    [EDIT] adding more functions

--- lambdas/handler.js
+++ lambdas/handler.js
@@ -5,6 +5,8 @@ const fileType = require('file-type');
 
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 const pg = require("pg");
+const fetch = require('request-promised').get;
+const post = require('request-promised').post;
 
 
 module.exports.hello = (event, context, callback) => {
@@ -52,6 +54,7 @@ module.exports.upload = (event, context, callback) => {
         Bucket: bucket,
         ContentEncoding: 'base64',
         ContentType: mime,
+        ACL: 'public-read',
         Metadata:{
           ContentType: mime,
           "user": username,
@@ -65,7 +68,7 @@ module.exports.upload = (event, context, callback) => {
         callback(null, {
           statusCode: '200',
           headers: {'Access-Control-Allow-Origin': '*'},
-          body: JSON.stringify({"message": "Successfully uploaded to S3"})
+          body: JSON.stringify({"message": "Successfully uploaded to S3", "filename":fileName})
       });
 
     });
@@ -99,36 +102,75 @@ module.exports.list = (event, context, callback) => {
 }
 
 module.exports.listFromS3 = (event, context, callback) => {
-
-  context.callbackWaitsForEmptyEventLoop = false;
-
-  const listQuery = `SELECT * FROM ${process.env.PG_TABLE} ORDER BY timestamp DESC LIMIT 20;`;
-  const client = new pg.Client(process.env.PG_CONNECTION_STRING);
-  client.connect();
-  const query = client.query(listQuery, (err, results) => {
-     client.end();
-
-    callback(null, {
-      statusCode: '200',
-      headers: {'Access-Control-Allow-Origin': '*'},
-      body: JSON.stringify({"results": results})
-    });
-
-  });  
   
-}
+    context.callbackWaitsForEmptyEventLoop = false;
+  
+    const params = {
+      Bucket: process.env.BUCKET,
+      MaxKeys: 10
+    };
+   s3.listObjects(params, function(err, data) {
+     if (err) callback(err, null); // an error occurred
+     else     callback(null, data);           // successful response
+   });
+    
+};
 
 module.exports.store = (event, context, callback) => {
   const connectionString = process.env.PG_CONNECTION_STRING;
   const tableName = process.env.PG_TABLE;
-
   const insert_query = `INSERT INTO ${tableName}(Timestamp, BucketKey, Username, Description, FileName, StaticLink) values($1, $2, $3, $4, $5, $6)`;
+  
   console.log(JSON.stringify(event, null, 2));
-  callback(null, {
-    statusCode: '200',
-    headers: {'Access-Control-Allow-Origin': '*'},
-    body: JSON.stringify({"results": event})
-  });
+
+
+  context.callbackWaitsForEmptyEventLoop = false;
+  
+  // const client = new pg.Client(connectionString);
+  // client.connect();
+
+  const file = event.Records[0].s3.object.key;
+  const url = `${process.env.BUCKET_URL}/${file}`;
+  console.log(url);
+
+  fetch(url)
+  .then( (res) => {
+    const headers =  JSON.parse(res.headers);
+    const user = headers['x-amz-meta-user'];
+    const description = headers['x-amz-meta-description'];
+    const options = {
+      uri: process.env.WRITE_URL,
+      body: JSON.stringify({
+        user,
+        description,
+        url,
+        title: 'some title because we forgot'
+      })
+    }
+
+    return post(options);
+  })
+  .then( res =>
+    callback(null, {
+      statusCode: '200',
+      headers: {'Access-Control-Allow-Origin': '*'},
+      body: JSON.stringify({"results": res.body})
+    })
+  );
+
+  // const query = client.query(insert_query,[event.timestamp, event.bucket, event.username, event.description, event.filename, event.staticurl], (err, results) => {
+  //   if(err) callback(new Error([err.statusCode], res.status(500).json({success: false, data: err} )));
+  //   client.end();
+
+  // });  
+
+  // callback(null, {
+  //   statusCode: '200',
+  //   headers: {'Access-Control-Allow-Origin': '*'},
+  //   body: JSON.stringify({"results": event})
+  // });
+
+
   // pg.connect(connectionString, (err, client, done) => {
   //   if(err) callback(new Error([err.statusCode], res.status(500).json({success: false, data: err} )));
   //
@@ -140,4 +182,22 @@ module.exports.store = (event, context, callback) => {
   //
   //     done();
 };
+
+module.exports.write = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  
+    const listQuery = `SELECT * FROM ${process.env.PG_TABLE} ORDER BY timestamp DESC LIMIT 20;`;
+    const client = new pg.Client(process.env.PG_CONNECTION_STRING);
+    client.connect();
+    const query = client.query(listQuery, (err, results) => {
+       client.end();
+  
+      callback(null, {
+        statusCode: '200',
+        headers: {'Access-Control-Allow-Origin': '*'},
+        body: JSON.stringify({"results": results})
+      });
+  
+    }); 
+}
 
